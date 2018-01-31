6b4b669e0666fe2edf7ad79af5aadfa80d11b98b
Jonathan Svenheden <jonathan@svenheden.com>
2018-01-31 19:31:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Invoice generator
+
+This is a tool for generating nice looking invoices from JSON documents. Print them in your favorite browser as PDF's and you're good to go.
+
+
+## How to use
+
+1. Clone this repo
+2. Populate `company.json` with your info
+3. Add a couple of invoices to the `invoice` folder. [You can see the format here](invoices/1.json)
+4. `npm install`
+5. `npm start`
+6. Go to `http://localhost:3000` in your browser and start printing or generating PDF's
+
+
+## License
+
+MIT © [Jonathan Persson](https://github.com/jonathanp)
---
+++ index.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const cons = require('consolidate');
+
+const app = express();
+
+app.engine('html', cons.mustache);
+app.set('view engine', 'html');
+app.set('views', __dirname);
+app.use(express.static(__dirname))
+
+const company = require('./company.json');
+const formatMoney = () => (text, render) => render(text).replace(/\B(?=(\d{3})+(?!\d))/g, ' ') + ' kr';
+
+app.get('/', (req, res, next) => {
+    const invoicesPath = path.join(__dirname, 'invoices');
+
+    fs.readdir(invoicesPath, (err, files) => {
+        const items = files
+            .map(file => file.replace(/.json/, ''))
+            .map(invoiceId => `<li><a href="/${invoiceId}">${invoiceId}</a></li>`);
+
+        res.send(`<ul>${items}</ul>`);
+    });
+});
+
+app.get('/:invoiceId', (req, res, next) => {
+    const invoiceId = req.params.invoiceId;
+    const invoicePath = path.join(__dirname, 'invoices', invoiceId + '.json');
+
+    fs.access(invoicePath, err => {
+        if (err) {
+            return next();
+        }
+
+        const invoice = require(invoicePath);
+        invoice.items = decorateItemsWithTotal(invoice.items);
+        invoice.total = calculateTotal(invoice.items);
+
+        res.render('invoice', {
+            company,
+            invoice,
+            title: `${invoiceId}`.padStart(3, '0'),
+            formatMoney
+        });
+    });
+});
+
+app.listen(3000, () => console.log('Invoice generator listening on port 3000'));
+
+const decorateItemsWithTotal = items => items.map(item => ({
+    ...item,
+    total: parseFloat(item.quantity) * item.price
+}));
+
+const calculateTotal = items => {
+    const exclVat = items.reduce((acc, item) => acc + item.total, 0);
+    const vat = exclVat * 0.25;
+    const inclVat = exclVat + vat;
+
+    return { exclVat, vat, inclVat };
+}
