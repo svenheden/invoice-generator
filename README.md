95bd6d7bd6993932f6d44efd95a430f8af3dedd9
Jonathan Svenheden <jonathan@svenheden.com>
2020-04-13 20:38:41 +0200

    Remove the need to manually "print as PDF" in browsers

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
 # Invoice generator
 
-This is a tool for generating nice looking invoices from JSON documents. Print them in your favorite browser as PDF's and you're good to go.
-
+This is a tool for generating nice looking PDF invoices from JSON documents.
 
 ## How to use
 
 1. Clone this repo
 2. Populate `company.json` with your info
-3. Add a couple of invoices to the `invoice` folder. [You can see the format here](invoices/1.json)
-4. `npm install`
-5. `npm start`
-6. Go to `http://localhost:3000` in your browser and start printing or generating PDF's
-
+3. `npm install`
+4. `npm run generate example.json`
 
 ## License
 
--- index.js
+++ index.js
@@ -1,71 +1,88 @@
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
-const cons = require('consolidate');
+const fs = require("fs").promises;
+const path = require("path");
+const puppeteer = require("puppeteer");
+const mustache = require("mustache");
 
-const app = express();
+const generateHtml = async (invoice) => {
+  const template = await fs.readFile("./template.html", "utf-8");
+  const company = require("./company.json");
 
-app.engine('html', cons.mustache);
-app.set('view engine', 'html');
-app.set('views', __dirname);
-app.use(express.static(__dirname))
+  if (company.logo) {
+    company.name = await fs.readFile(company.logo, "utf-8");
+  }
 
-const formatMoney = () => (text, render) => render(text).replace(/\B(?=(\d{3})+(?!\d))/g, ' ') + ' kr';
+  const itemsWithTotal = decorateItemsWithTotal(invoice.items);
 
-let company = require('./company.json');
+  const view = {
+    company,
+    invoice: {
+      ...invoice,
+      items: itemsWithTotal,
+      total: calculateTotal(itemsWithTotal),
+    },
+    formatMoney,
+  };
 
-company = {
-    ...company,
-    name: company.logo ? require(company.logo) : company.name
+  return mustache.render(template, view);
 };
 
-app.get('/', (req, res, next) => {
-    const invoicesPath = path.join(__dirname, 'invoices');
-
-    fs.readdir(invoicesPath, (err, files) => {
-        const items = files
-            .filter(file => /^[^\.]/.test(file))
-            .map(file => file.replace(/.json/, ''))
-            .map(invoiceId => `<li><a href="/${invoiceId}">${invoiceId}</a></li>`)
-            .join('');
-
-        res.send(`<ul>${items}</ul>`);
-    });
-});
-
-app.get('/:invoiceId', (req, res, next) => {
-    const invoiceId = req.params.invoiceId;
-    const invoicePath = path.join(__dirname, 'invoices', invoiceId + '.json');
-
-    fs.access(invoicePath, err => {
-        if (err) {
-            return next();
-        }
-
-        const invoice = require(invoicePath);
-        invoice.items = decorateItemsWithTotal(invoice.items);
-        invoice.total = calculateTotal(invoice.items);
-
-        res.render('invoice', {
-            company,
-            invoice,
-            title: `${invoiceId}`.padStart(3, '0'),
-            formatMoney
-        });
-    });
-});
-
-app.listen(3000, () => console.log('Invoice generator listening on port 3000'));
-
-const decorateItemsWithTotal = items => items.map(item => ({
+const decorateItemsWithTotal = (items) =>
+  items.map((item) => ({
     ...item,
-    total: parseFloat(item.quantity.replace(/,/, '.')) * item.price
-}));
+    total: parseFloat(item.quantity.replace(/,/, ".")) * item.price,
+  }));
 
-const calculateTotal = items => {
-    const exclVat = items.reduce((acc, item) => acc + item.total, 0);
-    const vat = exclVat * 0.25;
-    const inclVat = exclVat + vat;
+const calculateTotal = (items) => {
+  const exclVat = items.reduce((acc, item) => acc + item.total, 0);
+  const vat = exclVat * 0.25;
+  const inclVat = exclVat + vat;
 
-    return { exclVat, vat, inclVat };
-}
+  return { exclVat, vat, inclVat };
+};
+
+const formatMoney = () => (text, render) =>
+  render(text).replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " kr";
+
+const generatePdf = async (invoice, outputFilename) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const html = await generateHtml(invoice);
+
+  await page.setContent(html, { waitUntil: "networkidle0" });
+
+  await page.pdf({
+    path: outputFilename,
+    format: "A4",
+    margin: { top: "1cm", right: "1cm", bottom: "1cm", left: "1cm" },
+  });
+
+  await browser.close();
+};
+
+const main = async () => {
+  try {
+    const start = process.hrtime.bigint();
+    const invoicePath = process.argv[2];
+    const outputFilename = process.argv[3] || "invoice.pdf";
+
+    if (!invoicePath) {
+      throw new Error("An invoice JSON file is required");
+    }
+
+    const fileContents = await fs.readFile(invoicePath, "utf-8");
+    const invoice = JSON.parse(fileContents);
+
+    await generatePdf(invoice, outputFilename);
+
+    const end = process.hrtime.bigint();
+    const elapsed = (Number(end - start) / 1000000000).toFixed(4);
+
+    console.log(
+      `Successfully generated ${outputFilename} in ${elapsed} seconds`
+    );
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+main();
